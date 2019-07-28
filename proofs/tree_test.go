ed006ffb877cc9e5d2a26159abf74ee6035f5c5d
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-07-28 13:17:55 +0200

    public converter

--- proofs/tree_test.go
+++ proofs/tree_test.go
@@ -4,12 +4,10 @@ import (
 	"bytes"
 	"crypto/md5"
 	"crypto/sha256"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	proofspb "github.com/centrifuge/precise-proofs/proofs/proto"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 
@@ -1745,7 +1743,7 @@ func TestOptimizeProofs(t *testing.T) {
   ]
 }
 `
-	original, dr, err := convertJSONProofs(payload)
+	original, dr, err := ConvertJSONProofs(payload)
 	assert.NoError(t, err)
 	opt, err = OptimizeProofs(original,  dr, sha256.New())
 	assert.NoError(t, err)
@@ -1772,66 +1770,4 @@ func TestOptimizeProofs(t *testing.T) {
 		origHashesCount += len(original[i].SortedHashes)
 	}
 	fmt.Printf("Original[%d] -> Optimized[%d] with factor[%f]\n", origHashesCount, optHashesCount, float64(optHashesCount)/float64(origHashesCount))
-}
-
-func convertJSONProofs(jsonProof string) ([]*proofspb.Proof, []byte, error) {
-	type header struct {
-		DocumentRoot string `json:"document_root"`
-	}
-	type proofItem struct {
-		Property string `json:"property"`
-		Value string `json:"value"`
-		Salt string `json:"salt"`
-		Hash string `json:"hash"`
-		SortedHashes []string `json:"sorted_hashes"`
-	}
-	type proofPayload struct {
-		Header header `json:"header"`
-		FieldProofs []proofItem `json:"field_proofs"`
-	}
-	parsed := &proofPayload{}
-	err := json.Unmarshal([]byte(jsonProof), parsed)
-	if err != nil {
-		return nil, nil, err
-	}
-	docRoot, err := hex.DecodeString(strings.Replace(parsed.Header.DocumentRoot, "0x", "", -1))
-	if err != nil {
-		return nil, nil, err
-	}
-	converted := make([]*proofspb.Proof, len(parsed.FieldProofs))
-	for idx, item := range parsed.FieldProofs {
-		property, err := hex.DecodeString(strings.Replace(item.Property,"0x", "", -1))
-		if err != nil {
-			return nil, nil, err
-		}
-		value, err := hex.DecodeString(strings.Replace(item.Value,"0x", "", -1))
-		if err != nil {
-			return nil, nil, err
-		}
-		salt, err := hex.DecodeString(strings.Replace(item.Salt,"0x", "", -1))
-		if err != nil {
-			return nil, nil, err
-		}
-		hash, err := hex.DecodeString(strings.Replace(item.Hash,"0x", "", -1))
-		if err != nil {
-			return nil, nil, err
-		}
-		shb := make([][]byte, len(item.SortedHashes))
-		for idy, sh := range item.SortedHashes {
-			shh, err := hex.DecodeString(strings.Replace(sh,"0x", "", -1))
-			if err != nil {
-				return nil, nil, err
-			}
-			shb[idy] = shh
-		}
-		converted[idx] = &proofspb.Proof{
-			Property: &proofspb.Proof_CompactName{CompactName: property},
-			Value: value,
-			Salt: salt,
-			Hash: hash,
-			SortedHashes: shb,
-		}
-	}
-
-	return converted, docRoot, err
 }
